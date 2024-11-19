33cedde245adc4183fd818ad3571bcd92373ed5c
Rico Ardiansyah <167639579+blane187gt@users.noreply.github.com>
2024-11-19 00:48:22 -0800

    Update attentions.py

--- programs/rvc/lib/algorithm/attentions.py
+++ programs/rvc/lib/algorithm/attentions.py
@@ -1,7 +1,7 @@
 import math
 import torch
 
-from programs.applio_code.rvc.lib.algorithm.commons import convert_pad_shape
+from programs.rvc.lib.algorithm.commons import convert_pad_shape
 
 
 class MultiHeadAttention(torch.nn.Module):
